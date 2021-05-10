0f034baabeddf4e6d2b6121c00ed5e940e2cd3c7
Tom Leb <tomleb3@gmail.com>
2021-05-10 22:37:54 +0300

    clean-ups

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Switch, Route } from 'react-router-dom'
 import { Home } from './pages/Home.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
-import { TrackDetails } from './cmps/TrackDetails.jsx'
 
 export function App() {
   return (
     <main className="App">
       <AppHeader />
       <Switch>
-        <Route path="/track/:id" component={TrackDetails} />
         <Route path="/" component={Home} />
       </Switch>
     </main>
